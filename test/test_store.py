cbc6505f9884dcdb08e48a1b086cdf405096dd14
Daco <76890895+Daco2020@users.noreply.github.com>
2023-08-06 22:27:15 +0900

    refactor: dao 와 repo 통합 (#4)
    * refactor: dao 와 repo 통합
    
    * test: 테스트 코드 pytest 로 재작성
    
    * refactor: 사용하지 않는 코드 제거
    
    * test: 테스트 함수 이름 변경

--- test/test_store.py
+++ test/test_store.py
@@ -1,35 +1,25 @@
 import csv
+import pytest
 
+from pydantic import ValidationError
 from app.models import Content, User
 
 
-def test_content_loading() -> None:
+def test_valid_content_loading() -> None:
     with open("store/contents.csv", "r") as f:
         reader = csv.DictReader(f)
-        try:
-            for content in reader:
-                user_id = content["user_id"]
+        for content in reader:
+            try:
                 Content(**content)
-        except Exception as message:
-            print(
-                f"\033[31m------ error ------\n{user_id=}\n{content=}\n{message=}\n-------------------\033[0m"
-            )
+            except ValidationError:
+                pytest.fail("ValidationError should not occur!")
 
 
-def test_user_loading() -> None:
+def test_valid_user_loading() -> None:
     with open("store/users.csv", "r") as f:
         reader = csv.DictReader(f)
-        try:
-            for user in reader:
-                user_id = user["user_id"]
-                User(**user)
-        except Exception as message:
-            print(
-                f"\033[31m------ error ------\n{user_id=}\n{user=}\n{message=}\n-------------------\033[0m"
-            )
-
-
-if __name__ == "__main__":
-    test_content_loading()
-    test_user_loading()
-    print("test done.")
+        for user in reader:
+            try:
+                User(**user)  # type: ignore
+            except ValidationError:
+                pytest.fail("ValidationError should not occur!")
