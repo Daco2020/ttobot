41faa1195ef6656e227157c01381cf5c3072a756
Daco2020 <dacokim32@gmail.com>
2023-09-29 14:33:36 +0900

    build: 운영환경에 따른 동작 분리

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,3 @@
-import asyncio
 from app.client import SpreadSheetClient
 from fastapi import FastAPI, Request
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -16,29 +15,35 @@ async def health(request: Request) -> bool:
     return True
 
 
-@app.on_event("startup")
-async def startup():
-    # # 서버 저장소 동기화
-    # client = SpreadSheetClient()
-    # sync_store(client)
-    # client.create_log_file()
+if settings.ENV == "prod":
 
-    # # 업로드 스케줄러
-    # schedule = BackgroundScheduler(daemon=True, timezone="Asia/Seoul")
-    # schedule.add_job(scheduler, "interval", seconds=10, args=[client])
-    # schedule.start()
+    @app.on_event("startup")
+    async def startup():
+        # 서버 저장소 동기화
+        client = SpreadSheetClient()
+        sync_store(client)
+        client.create_log_file()
 
-    # 슬랙 소켓 모드 실행
-    await slack_handler.connect_async()
+        # 업로드 스케줄러
+        schedule = BackgroundScheduler(daemon=True, timezone="Asia/Seoul")
+        schedule.add_job(scheduler, "interval", seconds=10, args=[client])
+        schedule.start()
 
+        # 슬랙 소켓 모드 실행
+        await slack_handler.connect_async()
 
-def scheduler(client: SpreadSheetClient) -> None:
-    client.upload()
+    def scheduler(client: SpreadSheetClient) -> None:
+        client.upload()
 
+    @app.on_event("shutdown")
+    async def shutdown():
+        # 서버 저장소 업로드
+        client = SpreadSheetClient()
+        client.upload()
 
-@app.on_event("shutdown")
-async def shutdown():
-    # # 서버 저장소 업로드
-    # client = SpreadSheetClient()
-    # client.upload()
-    ...
+else:
+
+    @app.on_event("startup")
+    async def startup():
+        # 슬랙 소켓 모드 실행
+        await slack_handler.connect_async()
