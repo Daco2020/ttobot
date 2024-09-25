9a56b7914f951c40cdb868d1c21d6b52e20f105b
Daco2020 <dacokim32@gmail.com>
2024-09-25 15:45:08 +0900

    refactor: 종이비행기 API 로그인 로직 적용

--- app/api/views/paper_planes.py
+++ app/api/views/paper_planes.py
@@ -1,8 +1,10 @@
 from starlette import status
 from fastapi import APIRouter, Depends, Query
+from app.api.auth import current_user
 from app.api.deps import api_service
 from app.api.services import ApiService
 from app.api import dto
+from app.models import SimpleUser
 
 
 router = APIRouter()
@@ -14,14 +16,14 @@ router = APIRouter()
     response_model=dto.PaperPlaneResponse,
 )
 async def fetch_sent_paper_planes(
-    user_id: str,
     offset: int = 0,
     limit: int = Query(default=50, le=50),
     service: ApiService = Depends(api_service),
+    user: SimpleUser = Depends(current_user),
 ) -> dto.PaperPlaneResponse:
     """조건에 맞는 종이비행기를 가져옵니다."""
     count, data = service.fetch_sent_paper_planes(
-        user_id=user_id, offset=offset, limit=limit
+        user_id=user.user_id, offset=offset, limit=limit
     )
     return dto.PaperPlaneResponse(
         count=count, data=[each.model_dump() for each in data]
@@ -34,14 +36,14 @@ async def fetch_sent_paper_planes(
     response_model=dto.PaperPlaneResponse,
 )
 async def fetch_received_paper_planes(
-    user_id: str,
     offset: int = 0,
     limit: int = Query(default=50, le=50),
     service: ApiService = Depends(api_service),
+    user: SimpleUser = Depends(current_user),
 ) -> dto.PaperPlaneResponse:
     """조건에 맞는 종이비행기를 가져옵니다."""
     count, data = service.fetch_received_paper_planes(
-        user_id=user_id, offset=offset, limit=limit
+        user_id=user.user_id, offset=offset, limit=limit
     )
     return dto.PaperPlaneResponse(
         count=count, data=[each.model_dump() for each in data]
