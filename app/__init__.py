25c215a36ee84f5aaecea228e9f144a54f6bdb71
Daco2020 <dacokim32@gmail.com>
2024-09-25 12:55:51 +0900

    chore: aloow_origins 설정

--- app/__init__.py
+++ app/__init__.py
@@ -32,7 +32,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://geultto-post-board.netlify.app/",
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
